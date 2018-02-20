8ef8a6c6ff4a0139483b2c45ba79863f42a1bf55
Tiago Silva Pereira Rodrigues <tiagosilvapereira3@gmail.com>
2018-02-20 18:58:53 -0300

    Removing unecessary views

--- resources/views/home/clients/fields.blade.php
+++
@@ -1,8 +0,0 @@
-
-
-<div class="form-group col-xs-12 col-sm-6">
-    {!! Form::label('name', 'Nome') !!} <b class="asterisk required">*</b>
-    {!! Form::text('name', null, ['class' => 'form-control', 'required' => 'required']) !!}
-</div>
-
-
--- resources/views/home/clients/show_fields.blade.php
+++
@@ -1,5 +0,0 @@
-
-<div class="form-group">
-    {{ Form::label('name', 'Nome') }}
-    <p>{{ $client->name }}</p>
-</div>
